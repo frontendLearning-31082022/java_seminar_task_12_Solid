faf0d0fbd49f202672fb7ef82fca8ea90d74bef8
testoccgg <testoccgg@gmail.com>
2023-03-21 13:26:31 +0300

    added task_2

---
+++ src/task_2/task_2.java
@@ -0,0 +1,17 @@
+package task_2;
+
+import patterns.ImmutableClass;
+import patterns.Null_Object;
+import patterns.Producer_Consumer.Producer;
+import patterns.Producer_Consumer.Producer_Consumer;
+import patterns.Strategy;
+
+// Реализуйте паттерны: Null object, ImmutableClass, Стратегия и Producer-Consumer
+public class task_2 {
+    public static void main(String[] args) {
+        Null_Object.main(null);
+        ImmutableClass.main(null);
+        Strategy.main(null);
+        Producer_Consumer.main(null);
+    }
+}
